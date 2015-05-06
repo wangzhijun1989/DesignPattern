30fc87587964b1ec8a6181e05b83bd2a5a4a9eb3
wangzhijun1989 <wzhj0528@gmail.com>
2015-05-06 22:19:56 +0800

    重构代码结构

---
+++ test/src/com/jwork/dp/test/Test.java
@@ -0,0 +1,10 @@
+package com.jwork.dp.test;
+
+/**
+ * Created by Administrator on 2015/5/6.
+ */
+public class Test {
+    public static void main(String args[]){
+       System.out.println("test");
+    }
+}
